969f4dfdb03d6374aecd7743ebaf8b706767adc3
pangzixiang <313227220@qq.com>
2023-08-27 15:59:45 +0800

    bugfix

--- template/script/build-server.cjs
+++ template/script/build-server.cjs
@@ -7,6 +7,7 @@ function build() {
 
     fs.copyFileSync('./server/server.js', './dist/server.js')
     fs.copyFileSync('./server/proxy-config.json', './dist/proxy-config.json')
+    fs.copyFileSync('./server/server-options.json', './dist/server-options.json')
     fs.copyFileSync('./server/package.json', './dist/package.json')
     fs.copyFileSync('./server/package-lock.json', './dist/package-lock.json')
 }
